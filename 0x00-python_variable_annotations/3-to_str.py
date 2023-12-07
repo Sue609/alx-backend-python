cd8428775755177eed707aae9bce3d88fda89ee6
Sue609 <susankamu609@gmail.com>
2023-12-07 15:39:38 +0300

    type-annoted function that takes a float and returns a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+a basic annotation - to string
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    functionthat takes a float and returns a string
+    '''
+    return str(n)
