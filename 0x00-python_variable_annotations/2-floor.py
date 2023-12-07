a33eabbca2823cb16328f3dc85740bcfb44f4736
Sue609 <susankamu609@gmail.com>
2023-12-07 15:27:01 +0300

    type-annoted function that returns the floor of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Basic annotation - floor function
+'''
+import math
+
+
+def floor(n: float) -> float:
+    '''
+    type-annoted function that takes a float and returns a float
+    '''
+    return math.floor(n)
