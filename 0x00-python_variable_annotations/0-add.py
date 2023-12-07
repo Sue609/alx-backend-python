365c0668d2965e5c9378aba05a908a9a9db9c512
Sue609 <susankamu609@gmail.com>
2023-12-07 15:10:15 +0300

    basic type-annoted function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+This module introduces a function with basic annotation
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    type annoted function add
+    '''
+    return (a + b)
